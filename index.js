85c68ce52ab5a3ffce3e7264b7331e2f97d68cde
Liz Burton <liz.burton147@gmail.com>
2017-11-08 13:38:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value) {
+<<<<<<< HEAD
   return Object.assign({}, object, { [key]: value});
 }
 
@@ -18,4 +19,7 @@ function deleteFromObjectByKey(object, key) {
 function destructivelyDeleteFromObjectByKey(object, key) {
   delete object[key];
   return object;
+=======
+  Object.assign({}, {key: value});
+>>>>>>> c9e98dbd36a8e2343a6aaaea112ba9cd578e2f09
 }
